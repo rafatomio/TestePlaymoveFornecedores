08f19adf4d7ce496148f47e10feba0a1cfe3a89d
Rafael Tomio <rafatomio@hotmail.com>
2024-12-09 13:41:07 -0300

    Incrementações de crud

--- Fornecedores/Controllers/FornecedorController.cs
+++ Fornecedores/Controllers/FornecedorController.cs
@@ -36,5 +36,21 @@ namespace Fornecedor.Controllers
             FornecedorModel fornecedor = await repositorioFornecedor.Adicionar(fornecedorModel);
             return Ok(fornecedor);
         }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<FornecedorModel>> Atualizar([FromBody] FornecedorModel fornecedorModel, int id)
+        {
+            fornecedorModel.Id = id;
+            FornecedorModel fornecedor = await repositorioFornecedor.Atualizar(fornecedorModel, id);
+            return Ok(fornecedor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<FornecedorModel>> Apagar(int id)
+        {
+            bool apagado = await repositorioFornecedor.Apagar(id);
+            return Ok(apagado);
+        }
+
     }
 }
